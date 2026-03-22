9eaa84a5fbdb88c7e288ee103fba361e3dc6a779
Aaron <aaron_d_1@icloud.com>
2026-03-22 12:53:50 -0400

    Deploy valid VPC and subnet.

--- main.tf
+++ main.tf
@@ -2,15 +2,17 @@ provider "aws" {
   region = "us-east-2"
 }
 
-data "aws_ssm_parameter" "amzn-linux" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block       = "10.0.0.0/20"
+  instance_tenancy = "default"
+
+  tags = local.common_tags
 }
 
-resource "aws_instance" "web-server" {
-  ami           = data.aws_ssm_parameter.amzn-linux.value
-  instance_type = "t3.micro"
+resource "aws_subnet" "terraform_vpc_subnet" {
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = "10.0.0.0/24"
 
-  tags = {
-    Name = var.instance-name
-  }
+  tags = local.common_tags
+
 }
